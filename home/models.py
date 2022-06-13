eee1f6cf57d198d95614e6ca6c9fd8f5894fd8eb
Arkay <riyadh.k@outlook.com>
2022-06-13 17:02:38 +0000

    Add models to all apps

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Newsletter(models.Model):
+    """
+    Class that defines the Newsletter model
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254)
+    accept_policy = models.BooleanField(default=False)
+
+
+class ContactForm(models.Model):
+    """
+    Class that defines the ContactForm model
+    """
+    email = models.EmailField(max_length=254)
+    details = models.TextField()
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from products.models import Product
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    """
+    Class for the Wishlist model
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
