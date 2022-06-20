6105002b18a945f6c1a086774f21484e1b3f6d2f
Arkay <riyadh.k@outlook.com>
2022-06-20 14:25:34 +0000

    Edit Newsletter model

--- home/models.py
+++ home/models.py
@@ -6,9 +6,14 @@ class Newsletter(models.Model):
     """
     Class that defines the Newsletter model
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    email = models.EmailField(max_length=254)
-    accept_policy = models.BooleanField(default=False)
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    accepted_privacy_policy = models.BooleanField(default=False,
+                                                  blank=False, null=False)
+    registered_user = models.ForeignKey(
+                User, on_delete=models.SET_NULL, blank=True, null=True,
+                related_name='newsletter_subscriber')
+    created_on = models.DateTimeField(auto_now=True, editable=False)
+    updated_on = models.DateTimeField(auto_now=True, editable=False)
 
 
 class ContactForm(models.Model):
