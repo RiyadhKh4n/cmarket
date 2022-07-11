a1bf414e498fa581c70f07f52b1b029489fc85b6
Arkay <riyadh.k@outlook.com>
2022-07-11 13:05:16 +0000

    Update checkout files to test checkout_success

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,10 +1,3 @@
-"""
-To listen for signals from OrderLineItem - when instance of this model
-is saved or deleted. Then call the update_total method on the Order
-instance that the lineitem relates to, so that the totals in the Order
-instance get updated.
-"""
-
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
@@ -17,9 +10,8 @@ def update_on_save(sender, instance, created, **kwargs):
     """
     instance.order.update_total()
 
-
 @receiver(post_delete, sender=OrderLineItem)
-def delete_on_save(sender, instance, **kwargs):
+def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
--- checkout/views.py
+++ checkout/views.py
@@ -1,36 +1,13 @@
-
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
-from django.views.decorators.http import require_POST
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
-
 from products.models import Product
-# from profiles.models import UserProfile
-# from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
 
 import stripe
-import json
-
-
-@require_POST
-def cache_checkout_data(request):
-    try:
-        pid = request.POST.get('client_secret').split('_secret')[0]
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json.dumps(request.session.get('bag', {})),
-            'save_info': request.POST.get('save_info'),
-            'username': request.user,
-        })
-        return HttpResponse(status=200)
-    except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later.')
-        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -51,14 +28,9 @@ def checkout(request):
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
-
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.original_bag = json.dumps(bag)
-            order.save()
+            order = order_form.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -86,7 +58,6 @@ def checkout(request):
                     order.delete()
                     return redirect(reverse('view_bag'))
 
-            # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
@@ -107,25 +78,7 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        # # Attempt to prefill the form with any info the user maintains in their profile
-        # if request.user.is_authenticated:
-        #     try:
-        #         profile = UserProfile.objects.get(user=request.user)
-        #         order_form = OrderForm(initial={
-        #             'full_name': profile.user.get_full_name(),
-        #             'email': profile.user.email,
-        #             'phone_number': profile.default_phone_number,
-        #             'country': profile.default_country,
-        #             'postcode': profile.default_postcode,
-        #             'town_or_city': profile.default_town_or_city,
-        #             'street_address1': profile.default_street_address1,
-        #             'street_address2': profile.default_street_address2,
-        #             'county': profile.default_county,
-        #         })
-        #     except UserProfile.DoesNotExist:
-        #         order_form = OrderForm()
-        # else:
-        #     order_form = OrderForm()
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
