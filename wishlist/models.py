ee6df27516a1b074349965cc0d403bbe4aa5be01
Arkay <riyadh.k@outlook.com>
2022-07-18 16:59:14 +0000

    Make migrations to wishlist models.py

--- wishlist/models.py
+++ wishlist/models.py
@@ -3,11 +3,15 @@ from products.models import Product
 from django.contrib.auth.models import User
 
 
-class Wishlist(models.Model):
+class FavouritesList(models.Model):
     """
-    Class for the Wishlist model
+    Class based model for storing users favourited items.
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    image = models.ImageField(null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    product = models.ManyToManyField(
+        Product,
+        blank=True
+    )
