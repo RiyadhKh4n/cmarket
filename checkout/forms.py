52f8caeeb5547afa657a3119c48ed2ca13b0c874
Arkay <riyadh.k@outlook.com>
2022-07-29 13:21:01 +0000

    Change checkout/forms.py to conform to W3C Validator

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,40 +1,39 @@
-
 from django import forms
 from .models import Order
 
 
 class OrderForm(forms.ModelForm):
+    """form for customer details, delivery details"""
     class Meta:
+        """
+        Form based on Order model, but only need to display the fields
+        the user must complete. The others are calculated automatically.
+        """
         model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country',
                   'county',)
+        labels = {
+            'street_address1': 'Street address 1',
+            'street_address2': 'Street address 2',
+        }
 
     def __init__(self, *args, **kwargs):
         """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
+        Override init method to add placeholders for some fields
+        add class for CSS, set autofocus on first field
         """
         super().__init__(*args, **kwargs)
         placeholders = {
-            'full_name': 'Full Name',
-            'email': 'Email Address',
-            'phone_number': 'Phone Number',
+            'email': 'example@example.com',
             'postcode': 'Postal Code',
-            'town_or_city': 'Town or City',
-            'street_address1': 'Street Address 1',
-            'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
-
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if field != 'country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field in placeholders.keys():
+                self.fields[field].widget.attrs['placeholder'] = (
+                    placeholders[field]
+                    )
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].label = False
